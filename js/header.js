aacca9c517b4c173dd793cef988cd4135bb573a9
Lawrence <lawrence.0725@hotmail.com>
2023-05-23 13:01:14 +0700

    Add 餐點頁pop(桌號,商品加購,訂單加購,訂單折扣,訂單備註,商品備註,現金結帳,更多,登出)

--- js/header.js
+++ js/header.js
@@ -5,3 +5,46 @@ xhr.send();
 xhr.onreadystatechange=function(){
     if(xhr.readyState == 4 && xhr.status == 200){
         document.querySelectorAll("#header")[0].innerHTML = xhr.responseText; }};
+
+// Popup close
+function popClose(e) {
+    if (e.classList.contains('pop'))
+        window.onclick  = function(event) {
+            if (event.target == e) {
+                e.children[0].style.animationName = "popdown";
+                setTimeout(function(){
+                    e.style.display = "none";
+                    e.children[0].style.animationName = "popup";
+                }, 500); }}
+    else if (!(e.classList.contains('pop__body'))) {
+        e.closest('.pop').children[0].style.animationName = "popdown";
+        setTimeout(function(){
+            e.closest('.pop').style.display = "none";
+            e.closest('.pop').children[0].style.animationName = "popup";
+        }, 500); }}
+
+// 桌號popup
+function popTable() { document.getElementById("pop__table").style.display = "flex";}
+
+// 更多選單popup
+function popMore() { document.getElementById("pop__more").style.display = "flex";}
+
+// 更多選單popup
+function popLogout() { document.getElementById("pop__logout").style.display = "flex";}
+
+// 更多選單popup close
+function popMoreClose(e) {
+    if (e.classList.contains('pop'))
+        window.onclick  = function(event) {
+            if (event.target == e) {
+                e.children[0].style.animationName = "popleft";
+                setTimeout(function(){
+                    e.style.display = "none";
+                    e.children[0].style.animationName = "popright";
+                }, 500); }}
+    else if (!(e.classList.contains('pop__body'))) {
+        e.closest('.pop').children[0].style.animationName = "popleft";
+        setTimeout(function(){
+            e.closest('.pop').style.display = "none";
+            e.closest('.pop').children[0].style.animationName = "popright";
+        }, 500); }}
--- js/menu.js
+++ js/menu.js
@@ -15,5 +15,20 @@ function popClose(e) {
             e.closest('.pop').children[0].style.animationName = "popup";
         }, 500); }}
 
-// 通知popup
-function popNotice() { document.getElementById("pop__notice").style.display = "flex";}
+// 商品備註popup
+function popItemRemark() { document.getElementById("pop__item-remark").style.display = "flex";}
+
+// 訂單備註popup
+function popOrderRemark() { document.getElementById("pop__order-remark").style.display = "flex";}
+
+// 訂單折扣popup
+function popDiscount() { document.getElementById("pop__discount").style.display = "flex";}
+
+// 訂單加購popup
+function popOrderAddon() { document.getElementById("pop__order-addon").style.display = "flex";}
+
+// 商品加購popup
+function popItemAddon() { document.getElementById("pop__item-addon").style.display = "flex";}
+
+// 現金結帳popup
+function popCash() { document.getElementById("pop__cash").style.display = "flex";}
