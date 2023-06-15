2a4b21e69065f1305e5f514d5fcf895eacb38850
Rocio Hsu <shachien@gmail.com>
2023-06-15 20:43:32 +0800

    230615 update
    新增發票紀錄popup

---
+++ js/order.js
@@ -0,0 +1,9 @@
+// 發票紀錄popup
+function popReceipt() { document.getElementById("pop__receipt").style.display = "flex";}
+
+// 發票重開成功popup
+function popReceiptResend() { document.getElementById("pop__receipt--resend").style.display = "flex";}
+
+// 發票補印成功popup
+function popReceiptPrint() { document.getElementById("pop__receipt--print").style.display = "flex";}
+
