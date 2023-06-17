b6e4b99d7f3e74eb04e08ead11ab36031f24d8f2
Rocio Hsu <shachien@gmail.com>
2023-06-17 14:40:25 +0800

    230617 update
    新增調漲/已重印訂單/確定取消訂單/已取消訂單popup

--- js/order.js
+++ js/order.js
@@ -7,3 +7,17 @@ function popReceiptResend() { document.getElementById("pop__receipt--resend").st
 // 發票補印成功popup
 function popReceiptPrint() { document.getElementById("pop__receipt--print").style.display = "flex";}
 
+// 調帳popup
+function popAdjust() { document.getElementById("pop__adjust").style.display = "flex";}
+
+// 調帳成功popup
+function popAdjustResult() { document.getElementById("pop__adjust--result").style.display = "flex";}
+
+// 重印訂單popup
+function popOrderPrint() { document.getElementById("pop__order--print").style.display = "flex";}
+
+// 取消訂單確認popup
+function popOrderCancelConfirm() { document.getElementById("pop__order-cancel--confirm").style.display = "flex";}
+
+// 訂單已取消popup
+function popOrderCancelled() { document.getElementById("pop__order-cancelled").style.display = "flex";}
