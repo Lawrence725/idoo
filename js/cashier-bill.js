34937aade35c071ccf4343f2a2c23ba3fc51fa84
Lawrence <lawrence@dotdot.cc>
2024-01-25 22:19:58 +0800

    Add 結算

---
+++ js/cashier-bill.js
@@ -0,0 +1,21 @@
+// Popup close
+function popClose(e) {
+    if (e.classList.contains('pop'))
+        window.onclick  = function(event) {
+            if (event.target == e) {
+                e.children[0].style.animationName = "popdown";
+                setTimeout(function(){
+                    e.style.display = "none";
+                    e.children[0].style.animationName = "popup";
+                }, 500); }}
+    else if (!(e.classList.contains('pop__body'))) {
+        e.closest('.pop').children[0].style.animationName = "popdown";
+        setTimeout(function(){
+            e.closest('.pop').style.display = "none";
+            e.closest('.pop').children[0].style.animationName = "popup";
+        }, 500); }}
+
+// Popup open
+function popCash() { document.getElementById("pop__cash").style.display = "flex"; }
+function popQr() { document.getElementById("pop__qr").style.display = "flex"; }
+function popConfirm() { document.getElementById("pop__confirm").style.display = "flex"; }
